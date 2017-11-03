ca5dc37af89b68dc38eb1d312bc2988c06a9ffa7
soumadip <soumadiproy@gmail.com>
2017-11-03 15:36:02 +0530

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The cachematrix.R file contains two functions
+## 1. makeCacheMatric() and 
+## 2. cacheSolve() 
+## The first function creates an R object that 
+## stores a matrix and the inverse of the matirx
+## The second function cacheSolve() function retrieves the 
+## inverse of the matric from the cached value that is stored
+## in the universe of the makeCacheMatrix () Environment
 
-## Write a short comment describing this function
+
+## The function makeCacheMatrix initializes two (2) objects
+## they are x and invmtrx
+## and then it establishes functions called 
+## get() 
+## set() 
+## setinvrs()
+## getinvrs()
+## within set() <<- form of assignment operator is used which assigns value 
+##    on the right side of the operator to an object in the parent environment 
+##     named by the object on the left side of the operator 
+## get() functions uses the concept of lexical scoping features in R 
+##      x is not defined within get(), it is being retrieved from the parent environment
+## setinvrs() assigns the input argument to the value of invmtrx in the parent environment
+## and then getinvrs() defines get process for the inverse of the matrix
+## Each of these functions is assigned as an element within a list(), and is returned to the parent environment
 
 makeCacheMatrix <- function(x = matrix()) {
     invmtrx <- NULL
@@ -19,7 +40,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() is the function which uses the solve() function to get the inverse of the matrix
+## It first checks whether a new matrix has been passed, if not , the cached inverse of the matrix is returned
+## otherwise, it calculates the inverse and returns it 
+## !is.null(invmtrx) helps to check whether a new inverse needs to be calculated or it can be returned from the cache
+## this is depending on the statement invmtrx <<- NULL
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
